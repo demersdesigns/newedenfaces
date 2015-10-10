bf7946d7edebd23e90abeb2c4bc833b729342194
Paul Demers <paul@demersdesigns.com[mergetool Kaleidoscope]>
2015-10-10 15:43:37 -0400

    Footer.js work - next up, adding FooterActions.js

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ var express = require('express'),
     logger = require('morgan'),
     bodyParser = require('body-parser');
 
+var swig = require('swig'),
+    React = require('react'),
+    Router = require('react-router'),
+    routes = require('./app/routes');
+
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -10,6 +15,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res) {
+  Router.run(routes, req.path, function(Handler) {
+    var html = React.renderToString(React.createElement(Handler));
+    var page = swig.renderFile('views/index.html', { html: html });
+    res.send(page);
+  });
+});
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
